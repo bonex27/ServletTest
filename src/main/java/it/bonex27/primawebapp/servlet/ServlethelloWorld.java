f8f5fdfcf3d80e8126d3275d814c135a89453541
Pietro Bonechi <p.bonechi1@gmail.com>
2020-04-02 12:32:42 +0200

    transfer to maven & add mysql driver

---
+++ src/main/java/it/bonex27/primawebapp/servlet/ServlethelloWorld.java
@@ -0,0 +1,136 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.bonex27.primawebapp.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author pbone
+ */
+public class ServlethelloWorld extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, ClassNotFoundException, SQLException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        String DRIVER = "com.mysql.cj.jdbc.Driver";
+        try
+            {
+                Class.forName(DRIVER);
+            }
+            catch(ClassNotFoundException e)
+            {
+                System.out.println(e);
+            }
+        try {
+         String URL_mioDB = "jdbc:mysql://localhost:3306/fi_itis_meucci";
+         String query = "select * from class";
+         
+         Connection connessione = null;
+         connessione = DriverManager.getConnection(URL_mioDB, "bonex","pietro001");
+         
+         Statement statement = connessione.createStatement();
+         ResultSet resultset = statement.executeQuery(query);
+
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ServlethelloWorld</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            
+            while(resultset.next())
+         {
+             out.println("<h3>Classe: " + resultset.getString(3)+"</h3>");
+         }
+            out.print("<h1>Hello World!</h1>");
+            out.println("</body>");
+            out.println("</html>");
+            
+        } 
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+        finally {
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ServlethelloWorld.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(ServlethelloWorld.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ServlethelloWorld.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(ServlethelloWorld.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
